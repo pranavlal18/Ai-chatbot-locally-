17e93a5dfab322ecc25c83129d4abcaf51376b02
Pranav lal <pranavlal18@gmail.com>
2024-12-30 11:03:47 +0530

    used streamlit

--- chatbot.py
+++ chatbot.py
@@ -1,5 +1,7 @@
+import streamlit as st
 from langchain_ollama import OllamaLLM
 from langchain_core.prompts import ChatPromptTemplate
+
 
 template = """
 Answer the question:
@@ -7,21 +9,24 @@ Question: {question}
 Answer: 
 """
 
-model=OllamaLLM(model="llama3.2")
-prompt=ChatPromptTemplate.from_template(template)
+model = OllamaLLM(model="llama3.2")
+prompt = ChatPromptTemplate.from_template(template)
 chain = prompt | model
 
 
-
-def handle_conversation():
-    print("Ask me anything | enter exit")
-    while True:
-        user=input("You: ")
-        if user.lower() == "exit":
-            break
-        result=chain.invoke({"question":user})
-        print("Bot: ",result)
+st.title("Chatbot")
 
 
-if __name__=="__main__":
-    handle_conversation()
+user_input = st.text_input("Ask me anything:", "")
+
+
+if st.button("Get Response"):
+    if user_input.lower() == "exit":
+        st.write("Exiting...")
+    else:
+        
+        result = chain.invoke({"question": user_input})
+        
+        
+        st.write(f"**You**: {user_input}")
+        st.write(f"**Bot**: {result}")
